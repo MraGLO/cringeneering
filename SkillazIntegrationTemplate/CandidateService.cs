940c62365eca1be2e58eee0022c793c10b3daa4e
d.bahtenkov <d.bahtenkov@skillaz.ru>
2023-11-05 12:52:23 +0300

    initial

---
+++ SkillazIntegrationTemplate/CandidateService.cs
@@ -0,0 +1,68 @@
+﻿namespace HttpIntegrationTemplate;
+
+public class CandidateService
+{
+    private readonly HttpClient _httpClient;
+
+    public CandidateService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public void CheckCandidate(string id)
+    {
+        var candidate = GetCandidateInfo(id);
+        var vacancy = GetVacancyInfo(candidate.VacancyId);
+
+        if (CalculateMatching(candidate, vacancy))
+        {
+            AddCommentToCandidate(id, "Подходит");
+        }
+        else
+        {
+            AddCommentToCandidate(id, "Не подходит");
+        }
+    }
+    
+    private bool CalculateMatching(CandidateInfo candidateInfo, VacancyInfo vacancyInfo)
+    {
+        //todo: calculate matching for candidate and vacancy
+        return true;
+    }
+
+    public CandidateInfo GetCandidateInfo(string id)
+    {
+        //todo: use _httpClient for getting candidate
+        return new CandidateInfo();
+    }
+
+    private VacancyInfo GetVacancyInfo(string vacancyId)
+    {
+        //todo: use _httpClient for getting vacancy
+        return new VacancyInfo();
+    }
+
+    private void AddCommentToCandidate(string id, string text)
+    {
+        //todo: use _httpClient for sending comment 
+    }
+}
+
+public class CandidateInfo
+{
+    public string VacancyId { get; set; }
+    public List<CandidateNote> Notes { get; set; }
+    //todo: add properties for candidate
+}
+
+public class CandidateNote
+{
+    public string Id { get; set; }
+    public string Text { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
+
+public class VacancyInfo
+{
+    //todo: add properties for vacancy
+}
---
+++ SkillazIntegrationTemplate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Headers;
+using HttpIntegrationTemplate;
+
+var token = args.FirstOrDefault();
+if (string.IsNullOrEmpty(token))
+{
+    throw new Exception("Укажите токен");
+}
+
+var candidateId = args.LastOrDefault();
+
+if (string.IsNullOrEmpty(candidateId) || candidateId == token)
+{
+    throw new Exception("Укажите идентификатор кандидата");
+}
+
+var client = new HttpClient
+{
+    DefaultRequestHeaders = { Authorization = new AuthenticationHeaderValue("Bearer", token) }
+};
+
+var candidateService = new CandidateService(client);
+
+Console.WriteLine("Начинаем проверку кандидата");
+
+candidateService.CheckCandidate(candidateId);
+
+Console.WriteLine("Проверка окончена");
